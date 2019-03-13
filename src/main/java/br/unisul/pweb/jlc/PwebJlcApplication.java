f41547ea3d33b29f61d114b2b22c4145de110c0c
JLuizCardoso <julia.luizcardoso@bol.com.br>
2019-03-13 20:12:40 -0300

    Projeto criado

---
+++ src/main/java/br/unisul/pweb/jlc/PwebJlcApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.pweb.jlc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebJlcApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebJlcApplication.class, args);
+	}
+
+}
